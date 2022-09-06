1df9a9e5ec0bedf89e14349594795d046bb0dde3
Michal Procházka <michal@prochazka.ml>
2022-09-06 09:14:40 +0200

    Added "git push" replacement script which will autogen cache

---
+++ gitpush.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -d cache ]; then
+	echo ">>> Fresh working directory, fetching cache repo..."
+	git clone https://github.com/prochazkaml/CoachiumCached cache
+	cd cache
+	git remote set-url origin git@github.com:prochazkaml/CoachiumCached
+	cd ..
+fi
+
+echo ">>> Generating cache..."
+node node/compress.node.js cache
+
+gitver=`cat .git/refs/heads/master | cut -c1-7`
+
+echo ">>> Auto-commiting to cache repo..."
+cd cache
+git add -A .
+git commit -a -m "Syncing cache build with parent $gitver"
+echo ">>> Pushing cache repo..."
+git push
+
+echo ">>> Pushing parent repo..."
+cd ..
+git push
